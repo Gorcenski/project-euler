fee207325a94a62f454050f9fbac44b974cb1494
Emily Gorcenski <Gorcenski@users.noreply.github.com>
2021-01-31 21:50:47 +0100

    Update solutions for problems 1 and 2

--- problem_001.py
+++ problem_001.py
@@ -1,10 +1,8 @@
-from math import floor
-
 def solve():
-    gauss_sum_m = lambda n: lambda m: m * (floor((n -1) / m) + 1) * (floor((n - 1) / m)) / 2
-    gs = gauss_sum_m(1000)
-    print(int(gs(3) + gs(5) - gs(15)))
+    gauss_sum = lambda n : lambda m: m * ((n - 1) // m) * ((n - 1) // m + 1) // 2
+    gs = gauss_sum(1000)
+    return gs(3) + gs(5) - gs(15)
 
 
 if __name__ == "__main__":
-    solve()
+    print(solve())
--- problem_002.py
+++ problem_002.py
@@ -1,16 +1,18 @@
-from math import floor
 from numpy import convolve
-
 def solve():
     def fib():
-        a, b = 1, 2
+        a = 1
+        b = 2
         while True:
-            yield (a + b) ** 3
-            a, b = b, a + b
+            c = a + b
+            yield c ** 3
+            a = b
+            b = c
     f = fib()
-    f_cubed = [0, 1, 8]
-    f_cubed.extend(list(filter(lambda x: x < 4_000_000, [next(f) for i in range(10)])))
-    return sum(convolve(f_cubed, [1, 1, -1], mode='same')[:-1]) 
+    fib_cubed = [1, 1, 8]
+    fib_cubed.extend(list(filter(lambda x: x < 4_000_000,
+                                 [next(f) for i in range(10)])))
+    return sum(convolve(fib_cubed, [1, 1, -1], mode="same")[:-1])
 
 
 if __name__ == "__main__":
